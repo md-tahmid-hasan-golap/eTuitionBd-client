7689792b7b23fc18e977696205266ed15507e309
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-05-16 17:57:06 +0600

    add a dashborard layouts

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -16,6 +16,7 @@ const ErrorPage = () => {
 
   return (
     <div className="min-h-screen bg-[#F3F7FB] flex flex-col items-center justify-center p-6 font-sans">
+      {/* Content Wrapper */}
       <div className="flex flex-col items-center justify-center gap-8 md:gap-12 w-full max-w-5xl text-center">
         {/* Lottie Animation */}
         <MotionDiv
